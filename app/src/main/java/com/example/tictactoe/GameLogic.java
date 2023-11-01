ecd1060f8835f552988a027de0a0926e0e8ecac2
TIcTacToe <eranvil1603@gmail.com>
2023-11-01 17:40:08 +0200

    first commit

---
+++ app/src/main/java/com/example/tictactoe/GameLogic.java
@@ -0,0 +1,157 @@
+package com.example.tictactoe;
+
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+class GameLogic {
+    private int [][] gameBoard;
+
+    private Button playAgainBTN;
+    private Button homeBTN;
+    private TextView playerTurn;
+
+    private String[] playerNames = {"player 1","player 2"};
+    private int player = 1;
+
+    //1st element --> row, 2nd element --> col, 3rd element --> line type
+    private int[] winType = {-1,-1,-1};
+
+
+    GameLogic(){
+        gameBoard = new int [3][3];
+        for(int r=0;r<3;r++){
+            for(int c=0;c<3;c++){
+                gameBoard[r][c] = 0;
+            }
+        }
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    public boolean updateGameBoard(int row, int col){
+        if(gameBoard[row-1][col-1]==0){
+            gameBoard[row-1][col-1]=player;
+
+            if(player==1){
+                playerTurn.setText((playerNames[1]+"'s Turn"));
+            }
+            else{
+                playerTurn.setText((playerNames[0]+"'s Turn"));
+            }
+
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public void resetGame(){
+        for(int r=0;r<3;r++){
+            for(int c=0;c<3;c++){
+                gameBoard[r][c] = 0;
+            }
+        }
+        // updated player 1 to start a reset game
+        player = 1;
+        playAgainBTN.setVisibility(View.GONE);
+        homeBTN.setVisibility(View.GONE);
+        playerTurn.setText(playerNames[0]+"'s Turn");
+    }
+
+
+    public boolean winnerCheck(){
+        boolean isWinner = false;
+
+        // Horizontal check (winType==1)
+        for(int r=0; r<3;r++){
+            if(gameBoard[r][0] == gameBoard[r][1] && gameBoard[r][0] == gameBoard[r][2] && gameBoard[r][0] != 0){
+                winType = new int[] {r,0,1};
+                isWinner = true;
+            }
+        }
+        // Vertical check (winType==2)
+        for(int c=0; c<3;c++){
+            if(gameBoard[0][c] == gameBoard[1][c] && gameBoard[0][c] == gameBoard[2][c] && gameBoard[0][c] != 0){
+                winType = new int[] {0,c,2};
+                isWinner = true;
+            }
+        }
+        // Diagonal negative check (winType==3)
+        for(int d=0; d<1 ;d++){
+            if(gameBoard[d][d] == gameBoard[d+1][d+1] && gameBoard[d][d] == gameBoard[d+2][d+2] && gameBoard[d][d] != 0){
+                winType = new int[] {0,2,3};
+                isWinner = true;
+            }
+        }
+        // Diagonal positive check (winType==4)
+        for(int r=2; r>1;r--){
+            if(gameBoard[0][r] == gameBoard[r-1][r-1] && gameBoard[0][r] == gameBoard[r][0] && gameBoard[0][r] != 0){
+                winType = new int[] {2,2,4};
+                isWinner = true;
+            }
+        }
+
+        //checks how many spaces are filled in
+        int boardFilled = 0;
+        for(int r=0;r<3;r++){
+            for(int c=0;c<3;c++){
+                if(gameBoard[r][c]!=0)
+                    boardFilled++;
+            }
+        }
+
+        if(isWinner){
+            playAgainBTN.setVisibility(View.VISIBLE);
+            homeBTN.setVisibility(View.VISIBLE);
+            playerTurn.setText((playerNames[player-1]+"'s Won!"));
+            return true;
+        }
+        else if (boardFilled==9) {
+            playAgainBTN.setVisibility(View.VISIBLE);
+            homeBTN.setVisibility(View.VISIBLE);
+            playerTurn.setText(("Tie Game!"));
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
+    public void setPlayAgainBTN(Button playAgainBTN){
+        this.playAgainBTN=playAgainBTN;
+    }
+
+    public void setHomeBTN(Button homeBTN) {
+        this.homeBTN = homeBTN;
+    }
+
+    public void setPlayerTurn(TextView playerTurn) {
+        this.playerTurn = playerTurn;
+    }
+
+    public void setPlayerNames(String[] playerNames) {
+        this.playerNames = playerNames;
+    }
+
+
+    public int[][] getGameBoard(){
+        return gameBoard;
+    }
+    public void setPlayer(int player){
+        this.player=player;
+    }
+
+    public int getPlayer(){
+        return player;
+    }
+
+    public int[] getWinType() {
+        return winType;
+    }
+}
---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void playButtonClick(View view){
+        Intent intent = new Intent(this, PlayerSetUp.class);
+        startActivity(intent);
+    }
+
+}
